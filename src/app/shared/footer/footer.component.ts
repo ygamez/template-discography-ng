4219a25e61dae18b75b204cd85a504dc04f0f59a
ygamez <ernesto.yariel@gmail.com>
2022-12-13 18:45:28 +0100

    structure2.5

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
     test : Date = new Date();
+    date : Date = new Date();
 
     constructor() { }
 
